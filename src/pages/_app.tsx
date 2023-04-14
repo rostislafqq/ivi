84afaee147c1dd11510576649c18cec4d0a49005
Ilgiz <ilgizayupov7@gmail.com>
2023-04-14 18:23:30 +0500

    Найтроил базовые стили / шрифты

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
+import "@/config/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
 const Home: React.FC = () => {
-  return <>
-    <Head>
-        <title>Ivi - Главная</title>
-        <meta name="description" content="Ivi" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
+	return (
+		<>
+			<Head>
+				<title>Ivi - Главная</title>
+				<meta name="description" content="Ivi" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-    <h1>Home Page</h1>
-  </>
-}
+			<main>
+				<header id="page-header">
+					<div className="container">
+						<h1>Главная страница</h1>
+					</div>
+				</header>
+			</main>
+		</>
+	);
+};
 
-export default Home
+export default Home;
