7801f1fcdef32e888563671d564b7d09ff772b0f
Rastislav <35rtfds@mail.ru>
2023-04-21 22:28:50 +0700

    fix , add List , circle & sq for ListItem

--- src/components/atoms/ListItem/ListItem.tsx
+++ src/components/atoms/ListItem/ListItem.tsx
@@ -5,9 +5,9 @@ import styles from './ListItem.module.scss';
 
 import type { ListItemProps } from './ListItem.types';
 
-export const ListItem: React.FC<ListItemProps> = ({ className, children, type }) => {
+export const ListItem: React.FC<ListItemProps> = ({ className, children, type = '' }) => {
 	const listItemClasses = cn(styles.list, className, {
-		[styles[`list--${type}`]]: !!type,
+		[styles[`list-${type}`]]: !!type,
 	});
 
 	return <li className={listItemClasses}>{children}</li>;
