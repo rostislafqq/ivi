09253b209ab6cce4658cf3469ca572204bc54eb8
Алина Четкарёва <mosenka91@mail.ru>
2023-04-21 09:24:07 +0400

    fixed bugs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,9 +8,7 @@ const config: StorybookConfig = {
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
 	framework: {
 		name: '@storybook/nextjs',
-		options: {
-			nextConfigPath: path.resolve(__dirname, '../next.config.js'),
-		},
+		options: {},
 	},
 	docs: {
 		autodocs: 'tag',
