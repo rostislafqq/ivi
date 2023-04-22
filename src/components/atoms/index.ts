f920a63d9c4869da8be6f723070a9d8c345fd744
Rastislav <35rtfds@mail.ru>
2023-04-22 13:35:14 +0700

    added import to each component

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,6 @@
+import { Badge } from './Badge/Badge';
+import { Button } from './Button/Button';
+import { Heading } from './Heading/Heading';
+import { ListItem } from './ListItem/ListItem';
+
+export { ListItem, Heading, Button, Badge };
