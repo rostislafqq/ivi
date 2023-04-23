7b28ef80bca6e62fa0f5a1355dea91901003d3a9
Ilgiz <ilgizayupov7@gmail.com>
2023-04-24 03:30:51 +0500

    Fixed tests for Badge component

--- src/components/atoms/Badge/Badge.test.tsx
+++ src/components/atoms/Badge/Badge.test.tsx
@@ -5,90 +5,76 @@ import { Badge } from './Badge';
 
 describe('components/atoms/Badge', () => {
 	it('should render without props', () => {
-		const { getByText } = render(<Badge>Badge</Badge>);
+		const { container } = render(<Badge>Badge</Badge>);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toBeInTheDocument();
-		expect(badge).toHaveClass('badge');
+		expect(container.firstChild).toBeInTheDocument();
+		expect(container.firstChild).toHaveClass('badge');
 	});
 
 	it('should accept an additional class', () => {
-		const { getByText } = render(<Badge className="test-class">Badge</Badge>);
+		const { container } = render(<Badge className="test-class">Badge</Badge>);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toHaveClass('badge');
-		expect(badge).toHaveClass('test-class');
+		expect(container.firstChild).toHaveClass('badge');
+		expect(container.firstChild).toHaveClass('test-class');
 	});
 
 	it('should render small badge when size prop is "small"', () => {
-		const { getByText } = render(
+		const { container } = render(
 			<Badge className="test-class" size="small">
 				Badge
 			</Badge>,
 		);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toHaveClass('badge');
-		expect(badge).toHaveClass('test-class');
-		expect(badge).toHaveClass('badge--small');
+		expect(container.firstChild).toHaveClass('badge');
+		expect(container.firstChild).toHaveClass('test-class');
+		expect(container.firstChild).toHaveClass('badge--small');
 	});
 
 	it('should render normal badge when size prop is "normal"', () => {
-		const { getByText } = render(
+		const { container } = render(
 			<Badge className="test-class" size="normal">
 				Badge
 			</Badge>,
 		);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toHaveClass('badge');
-		expect(badge).toHaveClass('test-class');
-		expect(badge).toHaveClass('badge--normal');
+		expect(container.firstChild).toHaveClass('badge');
+		expect(container.firstChild).toHaveClass('test-class');
+		expect(container.firstChild).toHaveClass('badge--normal');
 	});
 
 	it('should render red badge when bg prop is "red"', () => {
-		const { getByText } = render(
+		const { container } = render(
 			<Badge className="test-class" bg="red">
 				Badge
 			</Badge>,
 		);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toHaveClass('badge');
-		expect(badge).toHaveClass('test-class');
-		expect(badge).toHaveClass('badge--red');
+		expect(container.firstChild).toHaveClass('badge');
+		expect(container.firstChild).toHaveClass('test-class');
+		expect(container.firstChild).toHaveClass('badge--red');
 	});
 
 	it('should render orange badge when bg prop is "orange"', () => {
-		const { getByText } = render(
+		const { container } = render(
 			<Badge className="test-class" bg="orange">
 				Badge
 			</Badge>,
 		);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toHaveClass('badge');
-		expect(badge).toHaveClass('test-class');
-		expect(badge).toHaveClass('badge--orange');
+		expect(container.firstChild).toHaveClass('badge');
+		expect(container.firstChild).toHaveClass('test-class');
+		expect(container.firstChild).toHaveClass('badge--orange');
 	});
 
 	it('should render gray badge when bg prop is "gray"', () => {
-		const { getByText } = render(
+		const { container } = render(
 			<Badge className="test-class" bg="gray">
 				Badge
 			</Badge>,
 		);
 
-		const badge = getByText('Badge');
-
-		expect(badge).toHaveClass('badge');
-		expect(badge).toHaveClass('test-class');
-		expect(badge).toHaveClass('badge--gray');
+		expect(container.firstChild).toHaveClass('badge');
+		expect(container.firstChild).toHaveClass('test-class');
+		expect(container.firstChild).toHaveClass('badge--gray');
 	});
 });
