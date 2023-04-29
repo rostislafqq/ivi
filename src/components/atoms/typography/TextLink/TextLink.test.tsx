c9d6805d7a4450635d01b6ba4fda236599c402d4
Ilgiz <ilgizayupov7@gmail.com>
2023-04-29 16:48:12 +0500

    Wrote test for TextLink component

--- src/components/atoms/typography/TextLink/TextLink.test.tsx
+++ src/components/atoms/typography/TextLink/TextLink.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 
 import { TextLink } from './TextLink';
@@ -16,5 +16,29 @@ describe('components/atoms/typography/TextLink', () => {
 		expect(container.firstChild).toBeInTheDocument();
 		expect(link).toBeInTheDocument();
 		expect(link).toHaveAttribute('href', 'https://google.com');
+	});
+
+	it('should calls onMouseOver and onMouseOut when mouse over and mouse out', () => {
+		const onMouseOverHandler = jest.fn();
+		const onMouseOutHandler = jest.fn();
+
+		const { getByRole } = render(
+			<TextLink
+				tag="div"
+				href="https://google.com"
+				onMouseOver={onMouseOverHandler}
+				onMouseOut={onMouseOutHandler}
+			>
+				Text Link text
+			</TextLink>,
+		);
+
+		const link = getByRole('link');
+
+		fireEvent.mouseOver(link);
+		expect(onMouseOverHandler).toHaveBeenCalledTimes(1);
+
+		fireEvent.mouseOut(link);
+		expect(onMouseOutHandler).toHaveBeenCalledTimes(1);
 	});
 });
