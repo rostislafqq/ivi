cdb29f88cb2e2480d8823d4bcbbd694a4197d647
Rastislav <35rtfds@mail.ru>
2023-05-24 16:52:58 +0700

    fix import problem

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,9 +1,9 @@
-import { FilmExtraInfo } from './FilmExtraInfo/FilmExtraInfo';
-import { Rating } from './Rating/Rating';
 import { Accordion } from './Accordion/Accordion';
+import { FilmExtraInfo } from './FilmExtraInfo/FilmExtraInfo';
 import { List } from './List/List';
 import { NavBar } from './NavBar/NavBar';
 import { NoAds } from './NoAds/NoAds';
+import { Rating } from './Rating/Rating';
 import { Support } from './Support/Support';
 
 import { Download } from '@/components/molecules/Share/Download/Download';
