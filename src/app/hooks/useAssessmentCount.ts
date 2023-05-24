dabbd2f5513be51b9b4df4fd8f04d9ccce37a363
Rastislav <35rtfds@mail.ru>
2023-05-24 17:04:02 +0700

    refactor new hook , px->rem on rating

--- src/app/hooks/useAssessmentCount.ts
+++ src/app/hooks/useAssessmentCount.ts
@@ -2,7 +2,7 @@ export const useAssessmentCount = (count: number) => {
 	if (count === 1) {
 		return '1 оценка';
 	}
-	if (count > 1 && count < 5) {
+	if (count % 10 > 1 && count % 10 < 5) {
 		return `${count} оценки`;
 	}
 	return `${count} оценок`;
--- src/components/molecules/Rating/Rating.stories.tsx
+++ src/components/molecules/Rating/Rating.stories.tsx
@@ -13,11 +13,34 @@ export default meta;
 
 type Story = StoryObj<typeof Rating>;
 
-export const large: Story = {
+export const largeGreen: Story = {
 	args: {
 		extra: 'Интересный сюжет',
 		assessment: 1234,
 		size: 'large',
+		rating: 7,
+	},
+};
+
+export const largeGray: Story = {
+	args: {
+		extra: 'Интересный сюжет',
+		assessment: 1234,
+		size: 'large',
+		rating: 6.9,
+	},
+};
+
+export const smallGreen: Story = {
+	args: {
+		size: 'small',
 		rating: 7.1,
+	},
+};
+
+export const smallGray: Story = {
+	args: {
+		size: 'small',
+		rating: 6.9,
 	},
 };
