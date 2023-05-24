b89601d57c48111b6ed5ec845027a39b2d4587bc
Rastislav <35rtfds@mail.ru>
2023-05-24 23:27:18 +0700

    add FilmIntroSection

--- src/components/molecules/Accordion/Accordion.tsx
+++ src/components/molecules/Accordion/Accordion.tsx
@@ -7,9 +7,15 @@ import { Button } from '@/components/atoms';
 
 import type { AccordionProps } from './Accordion.types';
 
-export const Accordion: React.FC<AccordionProps> = ({ children, length, buttonValues, buttonClass }) => {
-	const accordionClasses = cn(styles.lineClamp);
-	const openState = cn(styles.openAcc);
+export const Accordion: React.FC<AccordionProps> = ({
+	className = '',
+	children,
+	length,
+	buttonValues,
+	buttonClass,
+}) => {
+	const accordionClasses = cn(styles.lineClamp, className);
+	const openState = cn(styles.openAcc, className);
 
 	const [isOpen, setIsOpen] = useState(false);
 	const lineClampStyle = { '--line-clamp': `${length}` } as React.CSSProperties;
