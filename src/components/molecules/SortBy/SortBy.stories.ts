97b21ff5edb04736124c3e91191bd301a0e5ace0
Ilgiz <ilgizayupov7@gmail.com>
2023-06-08 06:25:34 +0500

    Added SortBy in Storybook

--- src/components/molecules/SortBy/SortBy.stories.ts
+++ src/components/molecules/SortBy/SortBy.stories.ts
@@ -5,4 +5,38 @@ import { SortBy } from './SortBy';
 const meta: Meta<typeof SortBy> = {
 	title: 'Molecules/SortBy',
 	component: SortBy,
-}
+	argTypes: {
+		options: {
+			description: 'Опции сортировки',
+		},
+		selectedOption: {
+			description: 'Выбранная опция сортировки',
+		},
+		setSelectedOption: {
+			type: 'function',
+			description: 'Функция выбора опции сортировки',
+		},
+		className: {
+			description: 'Добавить класс для Storybook',
+		},
+	},
+	tags: ['autodocs'],
+};
+
+export default meta;
+
+const options = [
+	{ label: 'названию', value: 'title' },
+	{ label: 'дате добавления', value: 'new' },
+	{ label: 'популярности', value: 'popular' },
+	{ label: 'рейтингу IMDB', value: 'imdb' },
+];
+
+type Story = StoryObj<typeof SortBy>;
+export const CollectionSort: Story = {
+	args: {
+		options,
+		selectedOption: options[0],
+		setSelectedOption: () => {},
+	},
+};
