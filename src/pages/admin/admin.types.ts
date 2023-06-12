aace8e05e69e97cb25afd81e435bbd29c03e33a1
Rastislav <35rtfds@mail.ru>
2023-06-12 15:34:36 +0700

    add new organism AdminAddFilm

--- src/pages/admin/admin.types.ts
+++ src/pages/admin/admin.types.ts
@@ -17,3 +17,26 @@ export interface BadgeAdmin {
 	id: number;
 	type: string;
 }
+
+export interface NewFilmData {
+	nameOriginal: string;
+	nameRu: string;
+	coverUrl: string;
+	logoUrl: string;
+	filmLength: number;
+	rating: number;
+	year: number;
+	ratingCount: number;
+	description: string;
+	ratingAgeLimits: string;
+	type: string;
+	status: string;
+	countries: { id: number }[];
+	genres: { id: number }[];
+	personsfilm: {
+		personId: string | number;
+		general: boolean;
+		role: number;
+	}[];
+	badge: number;
+}
